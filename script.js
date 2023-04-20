586cb819ec7922810252b9d75581d7d24a004a1e
Florestan Moulin <florestanmoulin@MacBook-Air-de-Florestan.local>
2023-04-20 16:06:39 +0200

    correction navigation js

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  if (slideIndex === 1 && n === -1) {
+    // si on est à la première slide et qu'on veut aller en arrière, aller à la dernière slide
+    showSlides(3);
+  } else if (slideIndex === 3 && n === 1) {
+    // si on est à la dernière slide et qu'on veut aller en avant, aller à la première slide
+    showSlides(1);
+  } else {
+    showSlides(slideIndex += n);
+  }
 }
 
 // Thumbnail image controls
@@ -15,8 +23,8 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > 3) {slideIndex = 1} // passer à la première slide s'il y a plus de 3 slides
+  if (n < 1) {slideIndex = 3} // passer à la dernière slide s'il y a moins de 1 slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
